d3e151d88b1f280fabe8b3c154a547633cb9147a
Feterovsky <afeterovskiy@gmail.com>
2015-04-13 23:38:49 -0400

    'first'

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<style>
+  body {background-color:lightgrey}
+  h1   {color:blue}
+  p    {color:#001452}
+  h2    {color:#FF3300}
+  a:link    {color:#000000; background-color:transparent; text-decoration:none}
+a:visited {color:#yellow; background-color:transparent; text-decoration:none}
+a:hover   {color:#ff0000; background-color:transparent; text-decoration:underline}
+a:active  {color:#ff0000; background-color:transparent; text-decoration:underline}
+</style>
+</head>
+
+<body>
+<h1>This is a heading</h1>
+<h2>This is a <br>custom <br>heading</h1>
+<p>This is a paragraph.</p>
+
+
+<?php // deletefile.php
+  if (!unlink('testfile2.new')) echo "Could not delete file";
+  else echo "File 'testfile2.new' successfully deleted";
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</br> 
+
+
+
+<a>Enter call duration</a><input type="text" id="callduration"></input> 
+<button onclick="document.getElementById('result').value = document.getElementById('callduration').value*10+50" >Calculate Bill</button>
+<br> <a>Result</a><input type="text" id="result" readonly="yes"></input>
+
+</body>
+
+</html>
---
+++ oksana.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<style>
+  body {background-color:lightblue}
+  h1   {color:darkblue}
+  p    {color:#001452}
+  h2    {color:#FF3300}
+  a:link    {color:#000000; background-color:transparent; text-decoration:none}
+a:visited {color:#yellow; background-color:transparent; text-decoration:none}
+a:hover   {color:#ff0000; background-color:transparent; text-decoration:underline}
+a:active  {color:#ff0000; background-color:transparent; text-decoration:underline}
+</style>
+</head>
+
+<body>
+<h1>Our Happy Family</h1>
+<h2>Oksana <br>Alex <br>Ilya <br>Daniel</h2>
+<p>Alex- lovely daddy, beloved husband, and hardworking architector</p>
+<p>Oksana- lovely mommy, beautiful wife,and student at Wake Tech</p>
+<p>Ilya- precious son, tender brother, smart student</p>
+<p>Daniel- sweet son, cute little brother</p>  
+
+<table border="1" style="width:50%">
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td> 
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td> 
+    <td>94</td>
+  </tr>
+</table>
+
+
+<a href="index.php" target="_blank">Link na Gugl</a>
+</body>
+
+</html>
